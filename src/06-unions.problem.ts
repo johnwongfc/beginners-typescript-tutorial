5bb3ef88d6c34e3dbd9fbe72607b10c90a8349fa
johnwongfc <wongjohn377@gmail.com>
2023-09-13 13:55:13 -0700

    finished up to 15

--- src/06-unions.problem.ts
+++ src/06-unions.problem.ts
@@ -8,13 +8,13 @@ interface User {
    * - 'user'
    * - 'super-admin'
    */
-  role: string;
+  role: 'admin' | 'user' | 'super-admin';
+  // role: true;
 }
 
 export const defaultUser: User = {
   id: 1,
-  firstName: "Matt",
-  lastName: "Pocock",
-  // @ts-expect-error
-  role: "I_SHOULD_NOT_BE_ALLOWED",
+  firstName: 'Matt',
+  lastName: 'Pocock',
+  role: 'admin',
 };
